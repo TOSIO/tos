f67098d88ed6b93d2dda1cb765666f57b8ccfcf9
johncao888 <john@toschain.org>
2018-05-18 18:25:51 +0800

    safemode added

---
+++ src/rpc/safemode.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 The Bitcoin Core developers
+// Copyright (c) 2017-2018 The Toschain Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TOSCHAIN_RPC_SAFEMODE_H
+#define TOSCHAIN_RPC_SAFEMODE_H
+
+static const bool DEFAULT_DISABLE_SAFEMODE = true;
+
+void ObserveSafeMode();
+
+#endif // TOSCHAIN_RPC_SAFEMODE_H
