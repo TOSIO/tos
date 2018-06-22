b558fb4c4f4afaef9a6901f7f49c848749eb1b8a
tos-timothy <timothy@toschain.org>
2018-06-22 17:53:25 +0800

    update params seeddata

--- src/chainparams.h
+++ src/chainparams.h
@@ -15,6 +15,12 @@
 #include <memory>
 #include <vector>
 
+struct CDNSSeedData {
+    std::string name, host;
+    bool supportsServiceBitsFiltering;
+    CDNSSeedData(const std::string &strName, const std::string &strHost, bool supportsServiceBitsFilteringIn = false) : name(strName), host(strHost), supportsServiceBitsFiltering(supportsServiceBitsFilteringIn) {}
+};
+
 struct SeedSpec6 {
     uint8_t addr[16];
     uint16_t port;
