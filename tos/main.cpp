7736f595901b5d823905b20e0c2c122994c6d3a8
poolchen <poolchen@toschain.org>
2018-07-10 18:27:58 +0800

    add tos

---
+++ tos/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <toscore/log/Log.h>
+
+ 
+int main(int argc, char const *argv[])
+{
+    /* code */
+printf("asdfasdf\n");
+//BOOST_LOG_INLINE_GLOBAL_LOGGER_CTOR_ARGS('dsfdfsd', 12);
+
+
+    return 0;
+}
