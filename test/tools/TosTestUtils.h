ee9a84c906a0da80749e15a21566fb4c031e1415
poolchen <poolchen@toschain.org>
2018-07-17 17:43:59 +0800

    modify test

---
+++ test/tools/TosTestUtils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <json/json.h>
+#include <toscore/log/Log.h>
+
+#include <boost/filesystem.hpp>
+
+namespace dev
+{
+namespace test
+{
+
+boost::filesystem::path getTestPath();
+
+
+}
+
+}
