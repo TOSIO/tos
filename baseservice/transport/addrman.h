6564178628ad44599c8fc9b4c79fb9a8f18213d2
lamchee <lamchee@sina.com>
2018-07-19 19:12:21 +0800

    update

--- baseservice/transport/addrman.h
+++ baseservice/transport/addrman.h
@@ -536,7 +536,7 @@ public:
             LOCK(cs);
             int err;
             if ((err=Check_()))
-                //LogPrintf("ADDRMAN CONSISTENCY CHECK FAILED!!! err=%i\n", err);
+                LogPrintf("ADDRMAN CONSISTENCY CHECK FAILED!!! err=%i\n", err);
         }
 #endif
     }
