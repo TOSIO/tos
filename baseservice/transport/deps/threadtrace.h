1d670fa67dd0549eb76a24bc70e5d6b4383ee23c
slchi <chisonglin@toschain.org>
2018-07-20 18:47:29 +0800

    update

---
+++ baseservice/transport/deps/threadtrace.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "log.h"
+#include <boost/thread/condition_variable.hpp> // for boost::thread_interrupted
+
+
+void RenameThread(const char* name);
+
+/**
+ * .. and a wrapper that just calls func once
+ */
+template <typename Callable> void TraceThread(const char* name,  Callable func)
+{
+    std::string s = strprintf("tos-%s", name);
+    RenameThread(s.c_str());
+    try
+    {
+        LogPrintf("%s thread start\n", name);
+        func();
+        LogPrintf("%s thread exit\n", name);
+    }
+    catch (const boost::thread_interrupted& )
+    {
+        LogPrintf("%s thread interrupt\n", name);
+        throw;
+    }
+    catch (const std::exception& e) {
+        PrintExceptionContinue(&e, name);
+        throw;
+    }
+    catch (...) {
+        PrintExceptionContinue(nullptr, name);
+        throw;
+    }
+}
