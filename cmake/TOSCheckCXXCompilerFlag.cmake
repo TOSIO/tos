8c53ab1de40634f58c8b47acc78da5d843ac7c6d
poolchen <poolchen@toschain.org>
2018-07-24 10:57:22 +0800

    modify project cmake  file

--- cmake/TOSCheckCXXCompilerFlag.cmake
+++ cmake/TOSCheckCXXCompilerFlag.cmake
@@ -8,7 +8,7 @@ include(CheckCXXCompilerFlag)
 #    if(check_cxx_compiler_flag(flag))
 #        add_compile_options(flag)
 #
-function(eth_add_cxx_compiler_flag_if_supported FLAG)
+function(tos_add_cxx_compiler_flag_if_supported FLAG)
   # Remove leading - or / from the flag name.
   string(REGEX REPLACE "^-|/" "" name ${FLAG})
   check_cxx_compiler_flag(${FLAG} ${name})
