55f2e669446a3401ec118091d87f950f0ec15534
yanglong <yangliong@toschain.org>
2018-08-01 14:21:18 +0800

    在FileUtil.h及其cpp文件里做了修改,添加了几个小函数

--- toscore/utils/FileUtils.cpp
+++ toscore/utils/FileUtils.cpp
@@ -1,9 +1,25 @@
 #include "FileUtils.h"
-#include "../jsonspirit/json_spirit.h"
-#include "../jsonspirit/json_spirit_reader_template.h"
-#include <map>
 #include <cstring>
 #include <fstream>
+
+
+json_spirit::Value_impl<json_spirit::Config_map<std::string> > getValue(boost::filesystem::path const &filePath)
+{
+    dev::Value_of_Map value;
+    std::string FileName = filePath.string();
+    std::ifstream is(FileName);
+    json_spirit::read(is, value);
+    return value;
+}
+
+ dev::ptree getContentOfIni(boost::filesystem::path const &filePath)
+ {
+     std::string FileName=filePath.string();
+     dev::ptree pt;
+     boost::property_tree::ini_parser::read_ini(FileName,pt);
+     return pt;
+ }
+
 /**
  *     读取 比如这样的json文件
  *     {"name":"admin","sex":"nan"}
@@ -11,13 +27,10 @@
  * */
 std::map<std::string, std::string> readJsonFile(boost::filesystem::path const &filePath)
 {
-    typedef json_spirit::Value_impl<json_spirit::Config_map<std::string> > Value_of_Map;
     std::map<std::string, std::string> Json_To_Map;
-    Value_of_Map value;
-    std::string fileName = filePath.string();
-    std::ifstream is(fileName);
-    json_spirit::read(is, value);
-    std::map<std::string, Value_of_Map>::iterator beg = value.get_obj().begin();
+    dev::Value_of_Map value;
+    value=getValue(filePath);
+    std::map<std::string, dev::Value_of_Map>::iterator beg = value.get_obj().begin();
     for (; beg != value.get_obj().end(); beg++)
     {
         Json_To_Map.insert(std::make_pair(beg->first, beg->second.get_str()));
@@ -46,19 +59,16 @@ std::string readStringFromJsonFile(boost::filesystem::path const &filePath, std:
 std::list<std::map<std::string, std::string> > readListFromJsonFile(boost::filesystem::path const &filePath, std::string key)
 {
 
-   typedef  json_spirit::Value_impl<json_spirit::Config_map<std::string> > Value_of_Map;
     std::list<std::map<std::string, std::string> > Json_To_List;
-    Value_of_Map value;
-    std::string fileName = filePath.string();
-    std::ifstream is(fileName);
-    json_spirit::read(is, value);
-    std::map<std::string, Value_of_Map>::iterator beg = value.get_obj().begin();
+    dev::Value_of_Map value;
+   value=getValue(filePath);
+    std::map<std::string, dev::Value_of_Map>::iterator beg = value.get_obj().begin();
     for (; beg != value.get_obj().end(); beg++)
     {
         if (strcmp(beg->first.c_str(), key.c_str()) == 0)
         {
 
-                std::map<std::string,Value_of_Map>::iterator iter= beg->second.get_obj().begin();
+                std::map<std::string,dev::Value_of_Map>::iterator iter= beg->second.get_obj().begin();
                  for(;iter!=beg->second.get_obj().end();iter++)
                  {
                         std::map<std::string, std::string> Json_To_Map;
@@ -71,24 +81,28 @@ std::list<std::map<std::string, std::string> > readListFromJsonFile(boost::files
 }
 
 /**
- *     读取 比如这样的json文件
- *     {"name":"admin","sex":"nan"}
+ *     ini:
+ *     [Section1]
+       Value1 = 10
+        Value2 = a_text_string
+ *
+ *       section.first=Section1
  *
  * */
 std::map<std::string, std::string> readConfFile(boost::filesystem::path const &filePath)
 {
-    typedef json_spirit::Value_impl<json_spirit::Config_map<std::string> > Value_of_Map;
-    std::map<std::string, std::string> Json_To_Map;
-    Value_of_Map value;
-    std::string fileName = filePath.string();
-    std::ifstream is(fileName);
-    json_spirit::read(is, value);
-    std::map<std::string, Value_of_Map>::iterator beg = value.get_obj().begin();
-    for (; beg != value.get_obj().end(); beg++)
+    std::map<std::string, std::string> Map_of_Conf;
+
+    dev::ptree pt=getContentOfIni(filePath);
+
+    for(auto& section :pt)
     {
-        Json_To_Map.insert(std::make_pair(beg->first, beg->second.get_str()));
+        for(auto &key:section.second)
+        {
+             Map_of_Conf.insert(std::make_pair(key.first,key.second.get_value<std::string>()));
+        }
     }
-    return Json_To_Map;
+    return Map_of_Conf;
 }
 
 //调用上面的函数
@@ -103,34 +117,29 @@ std::string readStringFromConfFile(boost::filesystem::path const &filePath, std:
 }
 
 /**
- *    读取 比如 这样的json文件     {"Package": { "Description": "Desription",
- *                                                               "Code":    "02",
- *                                                                 "Decription": "Description"  },
-                                             "x": { "Description": "Dcription",  "Code2": "022", "Decrip2tion": "Descrip2tion"  }}
+         ini:
+ *     [Section1]
+       Value1 = 10
+        Value2 = a_text_string
  *
+ *       section.first=Section1
  * */
-std::list<std::map<std::string, std::string>> readListFromConfFile(boost::filesystem::path const &filePath, std::string key)
+std::list<std::map<std::string, std::string>> readListFromConfFile(boost::filesystem::path const &filePath, std::string sectionName)
 {
-     typedef  json_spirit::Value_impl<json_spirit::Config_map<std::string> > Value_of_Map;
-    std::list<std::map<std::string, std::string> > Json_To_List;
-    Value_of_Map value;
-    std::string fileName = filePath.string();
-    std::ifstream is(fileName);
-    json_spirit::read(is, value);
-    std::map<std::string, Value_of_Map>::iterator beg = value.get_obj().begin();
-    for (; beg != value.get_obj().end(); beg++)
+     std::map<std::string, std::string> Map_of_Conf;
+    std::list<std::map<std::string, std::string>> list_of_MapValue;
+    dev::ptree pt=getContentOfIni(filePath);
+    for(auto& section :pt)
     {
-        if (strcmp(beg->first.c_str(), key.c_str()) == 0)
+        if(strcmp(section.first.c_str(),sectionName.c_str())==0)
         {
-
-                std::map<std::string,Value_of_Map>::iterator iter= beg->second.get_obj().begin();
-                 for(;iter!=beg->second.get_obj().end();iter++)
-                 {
-                        std::map<std::string, std::string> Json_To_Map;
-                        Json_To_Map.insert(std::make_pair(iter->first,iter->second.get_str()));
-                        Json_To_List.push_back(Json_To_Map);
-                 }
+            for(auto &key:section.second)
+           {
+             Map_of_Conf.insert(std::make_pair(key.first,key.second.get_value<std::string>()));
+           }
         }
+
     }
-    return Json_To_List;
+    list_of_MapValue.push_back(Map_of_Conf);
+    return list_of_MapValue;
 }
--- toscore/utils/FileUtils.h
+++ toscore/utils/FileUtils.h
@@ -3,35 +3,52 @@
 #include <string>
 #include <map>
 #include <list>
+#include "../jsonspirit/json_spirit.h"
+#include "../jsonspirit/json_spirit_reader_template.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 #include <boost/filesystem.hpp>
 
 
 
 namespace dev
 {
+ /*
+     起个别名，方便后面用,其实源码库里有一个别名的
+*/
+   typedef json_spirit::Value_impl<json_spirit::Config_map<std::string> > Value_of_Map;
 
-/* 
+   typedef  boost::property_tree::ptree  ptree;
+
+/*
+    get the value of the json file
+*/
+    json_spirit::Value_impl<json_spirit::Config_map<std::string> > getValue(boost::filesystem::path const &filePath);
+/*
     Read the json file to get the file content
     @param:filePath
     @return:std::map
  */
-std::map<std::string, std::string> readJsonFile(boost::filesystem::path const& filePath);
+   std::map<std::string, std::string> readJsonFile(boost::filesystem::path const& filePath);
 
-/* 
+/*
     Read the json file to get the file content
     @param:filePath
     @param:key
     @return:std::string
  */
-std::string readStringFromJsonFile(boost::filesystem::path const& filePath, std::string key);
+   std::string readStringFromJsonFile(boost::filesystem::path const& filePath, std::string key);
 
-/* 
+/*
     Read the json file to get the file content
     @param:filePath
     @param:key
     @return: std::list
  */
 std::list<std::map<std::string, std::string>> readListFromJsonFile(boost::filesystem::path const& filePath, std::string key);
+
+
+ boost::property_tree::ptree getContentOfIni(boost::filesystem::path const &filePath);
 
 /*
     Read the conf file to get the file content
@@ -40,7 +57,7 @@ std::list<std::map<std::string, std::string>> readListFromJsonFile(boost::filesy
 */
 std::map<std::string, std::string> readConfFile(boost::filesystem::path const& filePath);
 
-/* 
+/*
     Read the conf file to get the file content by key
     @param:filePath
     @param:key
@@ -48,13 +65,14 @@ std::map<std::string, std::string> readConfFile(boost::filesystem::path const& f
  */
 std::string readStringFromConfFile(boost::filesystem::path const& filePath, std::string key);
 
-/* 
+/*
     Read the conf file to get the file content by key
     @param:filePath
     @param:key
     @return: std::list
  */
-std::list<std::map<std::string, std::string>> readListFromConfFile(boost::filesystem::path const& filePath, std::string key);
+std::list<std::map<std::string, std::string>> readListFromConfFile(boost::filesystem::path const& filePath, std::string sectionName);
+
 
 
 }
