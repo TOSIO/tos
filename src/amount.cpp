aabc05c182cd27055b92ca36cbc176238a39a8b4
tos-qianjin <42207215+tos-qianjin@users.noreply.github.com>
2018-08-08 19:23:54 +0800

    update amount

--- src/amount.cpp
+++ src/amount.cpp
@@ -18,13 +18,13 @@ CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
         nSatoshisPerK = nFeePaid * 1000 / nSize;
     else
         nSatoshisPerK = 0;
+
 }
 
 CAmount CFeeRate::GetFee(size_t nBytes_) const
 {
     assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
     int64_t nSize = int64_t(nBytes_);
-
     CAmount nFee = nSatoshisPerK * nSize / 1000;
 
     if (nFee == 0 && nSize != 0) {
@@ -40,4 +40,5 @@ CAmount CFeeRate::GetFee(size_t nBytes_) const
 std::string CFeeRate::ToString() const
 {
     return strprintf("%d.%08d %s/kB", nSatoshisPerK / COIN, nSatoshisPerK % COIN, CURRENCY_UNIT);
+    
 }
