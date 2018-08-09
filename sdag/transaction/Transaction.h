84d34028582ef3e7da988dac7f4a7fbaf93f042b
poolchen <poolchen@toschain.org>
2018-08-09 16:24:26 +0800

    add sdag code

---
+++ sdag/transaction/Transaction.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "TransactionBase.h"
+
+
+namespace DEV
+{
+namespace sdag
+{
+
+class Transaction : public TransactionBase
+{
+
+
+    
+};
+
+} // namespace sdag
+} // namespace DEV
+
