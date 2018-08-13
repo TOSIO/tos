3ba4c6869105826a4530c7dd9b85fd9f5a7b37e9
tos-zhengjun <42345829+tos-zhengjun@users.noreply.github.com>
2018-08-13 20:52:53 +0800

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -87,6 +87,27 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
         return false;
+   
+
+    return true;
+}
+
+bool CheckProof_Of_Work(uint256 hash, unsigned int nBits, const Consensus::Params& params)
+{
+    bool fNegative;
+    bool fOverflow;
+    arith_uint256 bnTarget;
+
+    bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
+
+    // Check range
+    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
+        return false;
+
+    // Check proof of work matches claimed amount
+    if (UintToArith256(hash) > bnTarget)
+        return false;
+   
 
     return true;
 }
