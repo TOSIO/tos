945d3e9455af5a4fa4c16b226507e7a9c3b6d66d
TonyLee <tonylee@toschain.org>
2018-08-20 16:38:16 +0800

    Take the high level of hash to calculate pow

--- sdag/POW.h
+++ sdag/POW.h
@@ -5,7 +5,7 @@ using sdag_diff_t = u128;
 
 inline sdag_diff_t calculateWork(h256 hash)
 {
-    sdag_diff_t res = sdag_diff_t(hash);
+    sdag_diff_t res = sdag_diff_t(u256(hash) >> 128);
     sdag_diff_t max = -1;
     res >>= 32;
     return max / res;
