ecf7f1c9c1b3d06fe600a3cf08e3943908259273
zhangyutos <42205092+zhangyutos@users.noreply.github.com>
2018-09-05 10:33:36 +0800

    create Manager

---
+++ sdag/Manager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <toscore/common/Common.h>
+#include <toscore/common/Address.h>
+#include <boost/optional.hpp>
+#include <toscore/utils/RLP.h>
+#include <string>
+#include "block.h"
+
+namespace dev
+{
+namespace sdag
+{
+class Manager
+{
+
+  public:
+
+};
+
+} // namespace sdag
+} 
