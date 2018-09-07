4151ed63a59c9bbb733553de0fda49ca793714b5
zhangyutos <42205092+zhangyutos@users.noreply.github.com>
2018-09-07 13:00:11 +0800

    Update Manager.cpp

---
+++ sdag/Manager.cpp
@@ -0,0 +1,12 @@
+#include "Manager.h"
+#include <toscore/log/Log.h>
+#include <set>
+#define CONFIRM_NEED_VERIFY_BLOCK_NUM 32
+using namespace dev;
+using namespace dev::sdag;
+using namespace std;
+
+void Manager::addBlock(BlockRef block)
+{
+    
+}
