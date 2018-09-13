1490d9ce2a8cae667d2e2e4c6a2154ffd2472e73
tos-wuqi <42207638+tos-wuqi@users.noreply.github.com>
2018-09-13 19:33:10 +0800

    block.cpp

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -5,7 +5,6 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <primitives/block.h>
-
 #include <hash.h>
 #include <tinyformat.h>
 #include <utilstrencodings.h>
@@ -19,6 +18,7 @@ uint256 CBlockHeader::GetHash() const
 std::string CBlock::ToString() const
 {
     std::stringstream s;
+    
     s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
         GetHash().ToString(),
         nVersion,
@@ -26,9 +26,13 @@ std::string CBlock::ToString() const
         hashMerkleRoot.ToString(),
         nTime, nBits, nNonce,
         vtx.size());
+    
     for (const auto& tx : vtx) {
+        
         s << "  " << tx->ToString() << "\n";
+        
     }
+    
     return s.str();
 }
 
