a186afe8dfb6b5b17caa8643ff74bcf8fa46249e
tos-wuqi <42207638+tos-wuqi@users.noreply.github.com>
2018-09-19 20:06:01 +0800

    Update feerate.cpp

--- src/policy/feerate.cpp
+++ src/policy/feerate.cpp
@@ -7,7 +7,7 @@
 #include <policy/feerate.h>
 #include <tinyformat.h>
 const std::string CURRENCY_UNIT = "TOS";
-
+     
 CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
 {
     assert(nBytes_ <= uint64_t(std::numeric_limits<int64_t>::max()));
