21c4ac90813e91943057ecd7f83f58696419c686
zhangyutos <42205092+zhangyutos@users.noreply.github.com>
2018-10-11 12:04:56 +0800

    Update server.cpp

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -237,6 +237,7 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     // Event loop will exit after current HTTP requests have been handled, so
     // this reply will get back to the client.
     StartShutdown();
+    
     return "Toschain server stopping";
 }
 
