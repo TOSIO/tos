3ac55f6b9f51389c98ecb337f42cbacb0274a818
zhangyutos <42205092+zhangyutos@users.noreply.github.com>
2018-10-16 10:33:19 +0800

    Update Common.cpp

--- sdag/Common.cpp
+++ sdag/Common.cpp
@@ -8,7 +8,6 @@ using namespace dev;
 int64_t dev::sdag::get_timestamp()
 {
     struct timeval tp;
-
     gettimeofday(&tp, 0);
     return (int64_t)(unsigned long)tp.tv_sec << 10 | ((tp.tv_usec << 10) / 1000000);
 }
