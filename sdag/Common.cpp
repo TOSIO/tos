d7119ef1e6be6a13c69a17b2686507e56b787afd
tos-taojie <38199491+tos-taojie@users.noreply.github.com>
2018-10-19 21:20:49 +0800

    Update Common.cpp

--- sdag/Common.cpp
+++ sdag/Common.cpp
@@ -9,5 +9,6 @@ int64_t dev::sdag::get_timestamp()
 {
     struct timeval tp;
     gettimeofday(&tp, 0);
+    
     return (int64_t)(unsigned long)tp.tv_sec << 10 | ((tp.tv_usec << 10) / 1000000);
 }
